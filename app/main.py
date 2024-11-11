6b5227d44b45e7aef8416b589754352b5c69b975
elkofix <holasoygermamwow@gmail.com>
2024-11-10 21:56:27 -0500

    adding backend

---
+++ app/main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+from app.api.v1.api_v1 import router
+from starlette.middleware.cors import CORSMiddleware
+
+
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  # Permite solicitudes desde esta URL (cambia si usas otro puerto o dominio)
+    allow_credentials=True,
+    allow_methods=["*"],  # Permite todos los métodos HTTP (GET, POST, PUT, DELETE, etc.)
+    allow_headers=["*"],  # Permite todos los headers
+)
+
+
+app.include_router(router, prefix="/api/v1")
